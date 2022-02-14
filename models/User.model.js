d576b70aeddc43ab4895a83ff8a631f91d2408c7
yoricktf <yorick.tenfeld@gmail.com>
2022-02-14 15:05:15 +0100

    models completed

--- models/User.model.js
+++ models/User.model.js
@@ -5,9 +5,12 @@ const userSchema = new Schema(
   {
     username: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      unique: true
     },
     password: String,
+    profilePicture: String,
+    beerRuns: {type: mongoose.Schema.Types.ObjectId, ref: 'BeerRun'},
+    favorites: {type: mongoose.Schema.Types.ObjectId, ref: 'BeerRun'},
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
