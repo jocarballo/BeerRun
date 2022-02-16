fb5d7fbbad52b053b56dd0a20b97bd8b7b89d53f
Joana Carballo <joana16cruz@gmail.com>
2022-02-16 23:19:28 +0100

    search by address

--- routes/auth.js
+++ routes/auth.js
@@ -1,61 +1,59 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = new Router();
-const bcryptjs = require('bcryptjs');
+const bcryptjs = require("bcryptjs");
 const saltRounds = 10;
-const User = require('../models/User.model');
+const User = require("../models/User.model");
 // GET route ==> to display the signup form to users
 
 // POST route ==> to process form data
 
-
 // the get route skipped
 
 // POST route ==> to process form data
-router.post('/profile', (req, res, next) => {
+router.post("/profile", (req, res, next) => {
   // console.log("The form data: ", req.body);
 
   const { username, password } = req.body;
 
   bcryptjs
     .genSalt(saltRounds)
-    .then(salt => bcryptjs.hash(password, salt))
-    .then(hashedPassword => {
+    .then((salt) => bcryptjs.hash(password, salt))
+    .then((hashedPassword) => {
       return User.create({
         // username: username
         username,
         // passwordHash => this is the key from the User model
         //     ^
         //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
-        passwordHash: hashedPassword
+        passwordHash: hashedPassword,
       });
     })
-    .then(userFromDB => {
-      console.log('Newly created user is: ', userFromDB);
-      res.render('profile', {user: userFromDB})
+    .then((userFromDB) => {
+      console.log("Newly created user is: ", userFromDB);
+      res.render("profile", { user: userFromDB });
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
-router.post('/login', (req,res,next) => {
-   console.log('SESSION =====> ', req.session);
-  const {username, password} = req.body
-  User.findOne({username})
-  .then(userFromDB => {
+router.post("/login", (req, res, next) => {
+  console.log("SESSION =====> ", req.session);
+  const { username, password } = req.body;
+  User.findOne({ username }).then((userFromDB) => {
     if (!userFromDB) {
-      console.log("failed login")
-      res.render('index')
-      return
+      console.log("failed login");
+      res.render("index");
+      return;
     } else if (bcryptjs.compareSync(password, userFromDB.passwordHash)) {
       req.session.currentUser = userFromDB;
-      res.redirect('/profile');
+      res.redirect("/profile");
     }
-  })
-})
+  });
+});
 
 // router.post('/home', (req,res,next) => {
-  // req.session.destroy(err => {
-  //   if (err) next(err);
-  //   res.redirect('/home')
-  // })
+// req.session.destroy(err => {
+//   if (err) next(err);
+//   res.redirect('/home')
+// })
 // })
 module.exports = router;
