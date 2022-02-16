f96dc254c5ed94c8aaaa9d6848b4f91c822ec786
Joana Carballo <joana16cruz@gmail.com>
2022-02-16 13:15:47 +0100

    removed duplicate bars

--- routes/trip.js
+++ routes/trip.js
@@ -21,12 +21,13 @@ router.post('/trip/create', (req, res, next) => {
             longitude: endLongitudePoint
         }
     ).then(bars => {
+        console.log("filtered bars", bars);
         let startLocation = [startLongitudePoint, startLatitudePoint];
         let endLocation = [endLongitudePoint, endLatitudePoint];
 
         BeerRun.create({ name, bars, startLocation, endLocation })
     })
-    .then(res.redirect('/'))
+    .then(res.redirect('/home'))
     .catch(err => next(err));
 });
 
@@ -34,6 +35,14 @@ router.get('/trip/:id', (req, res, next) => {
     const id = req.params.id
     BeerRun.findById(id)
         .then(beerRun => {
+            let bars = beerRun.bars;
+
+            // extract information to center map
+            let barAtCenter = bars[Math.round((bars.length - 1) / 2)];
+            let centerCoordinates = barAtCenter.get("geometry").coordinates;
+            console.log("coordinates", centerCoordinates);
+
+            // create json to display markers
             let markersJson = {
                 type: 'FeatureCollection',
                 features: beerRun.bars
@@ -45,7 +54,8 @@ router.get('/trip/:id', (req, res, next) => {
              res.render('trip-details', 
                 { 
                     beerRun: beerRun,
-                    markersJsonStr: encodeURIComponent(markersJsonStr)
+                    markersJsonStr: encodeURIComponent(markersJsonStr),
+                    centerCoordinates: centerCoordinates
                  },
             )
          })
